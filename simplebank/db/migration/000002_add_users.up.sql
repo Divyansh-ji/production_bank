cd74da513ee1882f076420dd5bf7f9e9f450ec72
Divyansh-ji <Divyanshtiwary01@gmail.com>
2025-10-26 14:09:13 +0530

    add users

---
+++ simplebank/db/migration/000002_add_users.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "users" (
+  "username" VARCHAR PRIMARY KEY,
+  "hashed_password" VARCHAR NOT NULL,
+  "full_name" VARCHAR NOT NULL UNIQUE,
+  "password_changed_at" TIMESTAMPTZ NOT NULL DEFAULT '0001-01-01 00:00:00Z',
+  "created_at" TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+ALTER TABLE "accounts"
+ADD FOREIGN KEY ("owner") REFERENCES "users" ("username");
+
+CREATE UNIQUE INDEX ON "accounts" ("owner", "currency");
+
