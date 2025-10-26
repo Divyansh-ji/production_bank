64813bf864dc0a8e44d763ed68d088c67c2c27f0
Divyansh-ji <Divyanshtiwary01@gmail.com>
2025-10-27 00:43:58 +0530

    updated user things

--- simplebank/db/migration/000002_add_users.up.sql
+++ simplebank/db/migration/000002_add_users.up.sql
@@ -1,11 +1,11 @@
 CREATE TABLE "users" (
-  "username" VARCHAR PRIMARY KEY,
-  "hashed_password" VARCHAR NOT NULL,
-  "full_name" VARCHAR NOT NULL UNIQUE,
-  "password_changed_at" TIMESTAMPTZ NOT NULL DEFAULT '0001-01-01 00:00:00Z',
-  "created_at" TIMESTAMPTZ NOT NULL DEFAULT now()
+  "username" varchar PRIMARY KEY,
+  "hashed_password" varchar NOT NULL,
+  "full_name" varchar NOT NULL,
+  "email" varchar UNIQUE NOT NULL,
+  "password_changed_at" timestamptz NOT NULL DEFAULT('0001-01-01 00:00:00Z'),  
+  "created_at" timestamptz NOT NULL DEFAULT (now())
 );
-
 ALTER TABLE "accounts"
 ADD FOREIGN KEY ("owner") REFERENCES "users" ("username");
 
